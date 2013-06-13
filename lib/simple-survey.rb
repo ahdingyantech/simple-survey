6fd0671be8dc6eadcb10d4acb764e8feedda7e7e
fushang318 <fushang318@gmail.com>
2013-06-13 18:50:00 +0800

    fix

--- lib/simple-survey.rb
+++ lib/simple-survey.rb
@@ -8,17 +8,35 @@ require 'yaml'
 
 module SimpleSurvey
   if defined?(Rails)
-    TEMPLATE_PATH = Rails.root.join('config/survey_templates')
+
+    class Railtie < Rails::Railtie
+      initializer "simple-survey.setup_paths" do
+        SimpleSurvey::TEMPLATE_PATH = Rails.root.join('config/survey_templates')
+        SimpleSurvey::TEMPLATES = Dir["#{TEMPLATE_PATH}/*.yaml"].map do |yaml_path|
+          hash = YAML.load_file(yaml_path)
+          SurveyTemplate.new(
+            :id => hash['id'],
+            :name => hash['name'],
+            :template => hash['template']
+          )
+        end
+      end
+    end
+
   else
+
     TEMPLATE_PATH = File.expand_path('../../survey_templates', __FILE__)
+    TEMPLATES = Dir["#{TEMPLATE_PATH}/*.yaml"].map do |yaml_path|
+      hash = YAML.load_file(yaml_path)
+      SurveyTemplate.new(
+        :id => hash['id'],
+        :name => hash['name'],
+        :template => hash['template']
+      )
+    end
+    
   end
 
-  TEMPLATES = Dir["#{TEMPLATE_PATH}/*.yaml"].map do |yaml_path|
-    hash = YAML.load_file(yaml_path)
-    SurveyTemplate.new(
-      :id => hash['id'],
-      :name => hash['name'],
-      :template => hash['template']
-    )
-  end
 end
+
+
